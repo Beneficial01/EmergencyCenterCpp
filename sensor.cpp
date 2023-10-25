2997865599beb0d1c5b0631204714b23dece7568
Sudarshan Iyengar <sudarshan.iyengar@student.kuleuven.be>
2023-10-25 15:31:10 +0200

    Made Sensor Abstract and then added a few functions in the cpp for adding services to sensor and generate id

--- sensor.cpp
+++ sensor.cpp
@@ -1,6 +1,20 @@
 #include "sensor.h"
 
-Sensor::Sensor()
-{
+//Sensor::Sensor() {
 
+//}
+
+
+void Sensor::addService(std::shared_ptr<Service> service)
+{
+    services.push_back(service);
 }
+
+int Sensor::generateId()
+{
+    id = nextId;
+    nextId++;
+
+    return id;
+}
+
--- sensor.h
+++ sensor.h
@@ -4,31 +4,35 @@
 #include "service.h"
 #include "space.h"
 #include <vector>
+#include <memory>
 
 class EMERGENCYCENTRELIB_EXPORT Sensor: Space
 {
 public:
-    Sensor() = default;
+    //Sensor() = default;
     virtual ~Sensor() = default;
-    void activate() const;
-    void deactivate() const;
-    bool trigger() const;
-    void addService(Service & service );
-
+    virtual void activate() const;
+    virtual void deactivate() const;
+    virtual bool trigger() const;
+    virtual std::string toString() const;
+//    void addService(Service & service );
+    void addService(std::shared_ptr<Service> service );
 private:
+    //functions
     int generateId();
-
-
     //attributes
     bool activated;
     std::string vendorName;
     int id;
+    static int nextId;
     std::string startTime;
     std::string endTime;
     bool timerState;
     //std::shared_ptr<Location> location;
-    std::vector<Service> services;
+    //std::vector<Service> services;
+    std::vector<std::shared_ptr<Service>> services;
 };
 
+int Sensor::nextId = 1;
 
 #endif // SENSOR_H
