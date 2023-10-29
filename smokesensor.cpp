2d2a6b073133dfb113805fe37b046a5823b3cfad
Mahmoud Chairi <mahmoud.chairi@gmail.com>
2023-10-29 17:22:32 +0100

    Added locations to sensors

--- smokesensor.cpp
+++ smokesensor.cpp
@@ -8,7 +8,7 @@ SmokeSensor::SmokeSensor(std::string vendor, std::shared_ptr<Location> loc)
     vendorName = vendor;
     location = loc;
     std::cout << "Smoke Sensor constructor 1 called with Vendor  " << vendorName << std::endl;
-    std::cout << "in location : " << location << std::endl;
+    std::cout << "in : " << (*location).toString() << std::endl;
 }
 std::string SmokeSensor::getSensorType() const
 {
